a85541098b658a5e7fbe41708c13a94673c9c40f
Ricardo Salcido <ricardo.salcido@outlook.com>
2019-08-01 00:04:28 +0000

    lesson 14

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,21 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create]
 
+  
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @gram = Gram.new
   end
@@ -8,11 +23,15 @@ class GramsController < ApplicationController
   def index
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
 
   def create
@@ -24,10 +43,20 @@ class GramsController < ApplicationController
     end
   end
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
 
+  def render_not_found
+    render plain: "Not Found :(", status: :not_found 
+  end
 end
